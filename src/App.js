56b5c3565c59049cc9ec6baf49af785553e27fc5
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-06-04 05:13:02 +0530

    commented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Import React and Component from React, styels and components 
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
@@ -5,11 +6,16 @@ import SectionMain from './SectionMain';
 import Aside from './Aside';
 import Footer from './Footer';
 
+
+// creating a class component
 class App extends Component {
 
+//class component has a render() method followed by the return
   render() {
     return (
+//  returning all the components 
       <div className="App">
+//       Header has the style and width directly added
         <Header backColor="green" width="50%"></Header>
         <SectionMain></SectionMain>
         <Aside></Aside>
